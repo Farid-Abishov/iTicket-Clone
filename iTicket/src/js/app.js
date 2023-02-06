67b615726a4e7afb4eab0abbb715684d5ecda300
Farid-Abishov <abisovfarid14@gmail.com>
2023-02-06 16:20:44 +0400

    header section

---
+++ iTicket/src/js/app.js
@@ -0,0 +1,5 @@
+const  show=document.querySelector(".last_mid_link");
+const  content=document.querySelector(".last_link_cntnt");
+show.addEventListener("click", ()=>{
+    content.style.display="block";
+});
